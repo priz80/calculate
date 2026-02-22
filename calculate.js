87cef4624f37417da78c885ad08e09d9dae68264
IGOR <127472053+priz80@users.noreply.github.com>
2026-02-22 21:21:38 +0300

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,12 +1,15 @@
 document.getElementById("result").innerHTML = 68000;
 
 document.querySelectorAll("input").forEach((input) => {
-    input.addEventListener("input", () => {
-        sum = +document.getElementById("sum").value;
-        year = +document.getElementById("year").value;
-        per = +document.getElementById("per").value;
-        per = per / 100;
-        result = sum * per * year + sum;
-        document.getElementById("result").innerHTML = result;
-    });
-})
+  input.addEventListener("input", () => {
+    sum = +document.getElementById("sum").value;
+    year = +document.getElementById("year").value;
+    per = +document.getElementById("per").value;
+    if (sum >= 0 && year >= 0 && per >= 0) {
+      result = (per / 100) * sum * year + sum;
+    } else {
+      location.reload();
+    }
+    document.getElementById("result").innerHTML = result;
+  });
+});
